90662940401202c655623fe3959515a64b37992a
Samuel Anderson <36518796+mranderson455@users.noreply.github.com>
2018-11-02 08:45:26 -0400

    Aded chemicals

--- calculator.py
+++ calculator.py
@@ -7,6 +7,22 @@ import time
 ab_num = 6.02e23
 mol_liter = 22.4
 
+print("""
+                  _____      _            _       _              ____   _____ 
+                 / ____|    | |          | |     | |            / __ \ / ____|
+                | |     __ _| | ___ _   _| | __ _| |_ ___  _ __| |  | | (___  
+                | |    / _` | |/ __| | | | |/ _` | __/ _ \| '__| |  | |\___ \ 
+                | |___| (_| | | (__| |_| | | (_| | || (_) | |  | |__| |____) |
+Booting up...    \_____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   \____/|_____/ 
+""")
+print(".")
+time.sleep(random.random() * 1.5)
+print(".")
+time.sleep(random.random() * 1.5)
+print(".")
+time.sleep(random.random() * 1.5)
+print("")
+
 def get_input():
     cmd = input("::: ")
     if cmd == "a" or cmd == "A" or cmd == "add" or cmd == "addition" or cmd == "+":
@@ -45,13 +61,11 @@ def get_input():
         print('''Commands:
 h - help
 dance - do a lil dance :)
-
 Operators: 
 a - add
 s - subtract
 d - divide
 m - multiply
-
 Chemistry: 
 mm - calculate molar mass of element
 gf - grams to F.U
@@ -188,6 +202,98 @@ gl - grams to liters
             x = pd.He.mass
             elemass = float(x) * float(y)
             name = "He"
+        elif x == "Li":
+            x = pd.Li.mass
+            elemass = float(x) * float(y)
+            name = "Li"
+        elif x == "Be":
+            x = pd.Be.mass
+            elemass = float(x) * float(y)
+            name = "Be"
+        elif x == "B":
+            x = pd.B.mass
+            elemass = float(x) * float(y)
+            name = "B"
+        elif x == "C":
+            x = pd.C.mass
+            elemass = float(x) * float(y)
+            name = "C"
+        elif x == "N":
+            x = pd.N.mass
+            elemass = float(x) * float(y)
+            name = "N"
+        elif x == "O":
+            x = pd.O.mass
+            elemass = float(x) * float(y)
+            name = "O"
+        elif x == "F":
+            x = pd.F.mass
+            elemass = float(x) * float(y)
+            name = "F"
+        elif x == "Ne":
+            x = pd.Ne.mass
+            elemass = float(x) * float(y)
+            name = "Ne"
+        elif x == "Na":
+            x = pd.Na.mass
+            elemass = float(x) * float(y)
+            name = "Na"
+        elif x == "Mg":
+            x = pd.Mg.mass
+            elemass = float(x) * float(y)
+            name = "Mg"
+        elif x == "Al":
+            x = pd.Al.mass
+            elemass = float(x) * float(y)
+            name = "Al"
+        elif x == "Si":
+            x = pd.Si.mass
+            elemass = float(x) * float(y)
+            name = "Si"
+        elif x == "P":
+            x = pd.P.mass
+            elemass = float(x) * float(y)
+            name = "P"
+        elif x == "S":
+            x = pd.S.mass
+            elemass = float(x) * float(y)
+            name = "S"
+        elif x == "Cl":
+            x = pd.Cl.mass
+            elemass = float(x) * float(y)
+            name = "Cl"
+        elif x == "Ar":
+            x = pd.Ar.mass
+            elemass = float(x) * float(y)
+            name = "Ar"
+        elif x == "K":
+            x = pd.K.mass
+            elemass = float(x) * float(y)
+            name = "K"
+        elif x == "Ca":
+            x = pd.Ca.mass
+            elemass = float(x) * float(y)
+            name = "Ca"
+        elif x == "Sc":
+            x = pd.Sc.mass
+            elemass = float(x) * float(y)
+            name = "Sc"
+        elif x == "Ti":
+            x = pd.Ti.mass
+            elemass = float(x) * float(y)
+            name = "Ti"
+        elif x == "V":
+            x = pd.V.mass
+            elemass = float(x) * float(y)
+            name = "V"
+        elif x == "Cr":
+            x = pd.Cr.mass
+            elemass = float(x) * float(y)
+            name = "Cr"
+        elif x == "Mn":
+            x = pd.Mn.mass
+            elemass = float(x) * float(y)
+            name = "Mn"
         print("The molar mass of " + str(name) + str(y) + " is " + str(round(elemass, 4)) + " grams.")
         print("")
         get_input()
@@ -197,34 +303,40 @@ gl - grams to liters
         z = float(x) / float(y) * ab_num
         print(round(z, 5))
         print("")
+        get_input()
     elif cmd == "fg" or cmd == "FG":
         x = input("Number of F.U: ")
         y = input("Molar mass of element/compound: ")
         z = float(x) / ab_num * float(y)
         print(round(z, 7))
         print("")
+        get_input()
     elif cmd == "lf" or cmd == "LF":
         x = input("Number of liters: ")
         z = float(x) / mol_liter * ab_num
         print(round(z, 7))
         print("")
+        get_input()
     elif cmd == "fl" or cmd == "FL":
         x = input("Number of F.U: ")
         z = float(x) / ab_num * mol_liter
         print(round(z, 7))
         print("")
+        get_input()
     elif cmd == "lg" or cmd == "LG":
         x = input("Number of liters: ")
         y = input("Molar mass of element/compound: ")
         z = float(x) / mol_liter * float(y)
         print(round(z, 7))
         print("")
+        get_input()
     elif cmd == "gl" or cmd == "GL":
         x = input("Number of grams: ")
         y = input("Molar mass of element/compound: ")
         z = float(x) / float(y) * mol_liter
         print(round(z, 7))
         print("")
+        get_input()
     else:
         print("Invalid command.")
         print("")
